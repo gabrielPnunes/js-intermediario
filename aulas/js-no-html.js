a0238609421755d7713eae5e4459917d2b09b346
gabrielPnunes <95189995+gabrielPnunes@users.noreply.github.com>
2023-07-05 19:36:16 -0300

    Add files via upload

---
+++ aulas/js-no-html.js
@@ -0,0 +1,14 @@
+document.head;
+console.log(document);
+console.log(document.head);
+
+let ps = document.getElementsByClassName("paragrafos");
+console.log(ps)
+
+let p1 = document.getElementById("p1");
+console.log(p1)
+
+let tp = document.getElementsByTagName("p")
+console.log(tp)
+
+
