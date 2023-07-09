4ba6ca9ef16ed0546011dab2112bba0284c4236d
gabrielPnunes <95189995+gabrielPnunes@users.noreply.github.com>
2023-07-08 23:07:44 -0300

    Add files via upload

---
+++ aulas/events.js
@@ -0,0 +1,13 @@
+function h1click(){
+    console.log("executando a função");
+    }
+    
+    function changeh1(t){
+    let h1 = document.getElementsByTagName("h1")[0];
+    h1.innerText = t.value;
+    }
+    
+    function hide(){
+        let sumiu = getElementsById("sumiu")
+        sumiu.style.display = "none";
+    }
